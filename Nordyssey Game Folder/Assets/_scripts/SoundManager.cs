8ef9d8e6819a119db3e8cc3d4fe07df01eec6b71
SunniJB <333560@student.nord.no>
2023-01-17 21:28:05 +0100

    Made the sound work

--- Nordyssey Game Folder/Assets/_scripts/SoundManager.cs
+++ Nordyssey Game Folder/Assets/_scripts/SoundManager.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public List<AudioClip> soundClips;
-    private AudioSource audioSource;
+    public AudioSource audioSource;
     public AudioClip currentClip;
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
 
     public void PlaySound(string soundName)
     {
