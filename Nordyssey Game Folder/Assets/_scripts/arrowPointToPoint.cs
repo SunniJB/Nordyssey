73f61728cac0b6f89cdef76a6377cbf7ca6f2bc8
SunniJB <40769150+SunniJB@users.noreply.github.com>
2023-01-19 13:26:41 +0100

    Made an arrow that points in the right direction

---
+++ Nordyssey Game Folder/Assets/_scripts/arrowPointToPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowPointToPoint : MonoBehaviour
+{
+    public GameObject waypoint;
+    public Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    public void Update()
+    {
+        waypoint = GameObject.FindGameObjectWithTag("waypoint");
+        gameObject.transform.LookAt(waypoint.transform, cam.transform.up);
+    }
+
+}
