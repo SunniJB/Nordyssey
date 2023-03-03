ada2a50a83157cfb5151d250f8334cd710c403b7
Moeaatlo02 <jorgen.aatlo@gmail.com>
2023-03-03 10:37:57 +0100

    FrameLockScript

---
+++ Nordyssey Game Folder/Assets/_scripts/FrameLock.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class FrameLock : MonoBehaviour
+{
+
+    private int FramesPerSec;
+    private float frequency = 1.0f;
+    private string fps;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 30;
+    }
+
+
+    void Start()
+    {
+        StartCoroutine(FPS());
+    }
+
+    private IEnumerator FPS()
+    {
+        for (; ; )
+        {
+            // Capture frame-per-second
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(frequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            // Display it
+
+            fps = string.Format("FPS: {0}", Mathf.RoundToInt(frameCount / timeSpan));
+        }
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 100, 10, 150, 20), fps);
+    }
+}
