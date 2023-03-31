f9c2405d655ea9a211063ba9d84bbb1357beb132
SunniJB <40769150+SunniJB@users.noreply.github.com>
2023-03-31 13:12:41 +0200

    Update InputFieldStayUp.cs

--- Nordyssey Game Folder/Assets/_scripts/UI scripts/InputFieldStayUp.cs
+++ Nordyssey Game Folder/Assets/_scripts/UI scripts/InputFieldStayUp.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class InputFieldStayUp : MonoBehaviour
 {
     public Animator groupAnimator;
+
+    private void Start()
+    {
+        groupAnimator = GameObject.Find("Group").GetComponent<Animator>();
+    }
     public void OnInput()
     {
         if (gameObject.GetComponent<InputField>().text != null)
