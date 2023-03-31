18d323c7c0a349f8ce94d39113db546ecb5e72b4
hoangyenpham51 <347913@student.nord.no>
2023-03-31 11:09:41 +0200

    Update the tutorial

--- Nordyssey Game Folder/Assets/_scripts/UI scripts/HelpCanvas.cs
+++ Nordyssey Game Folder/Assets/_scripts/UI scripts/HelpCanvas.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class HelpCanvas : MonoBehaviour
 {
-        public GameObject help1, help2, help3;
+        public GameObject help1, help2, help3, helpBg;
         public LocationListScript locationListScript;
 
 
         public void activateHelp1()
         {
+        helpBg.SetActive(true);
         help1.SetActive(true);
         }
         public void activateHelp2()
         {
+        help1.SetActive(false);
+        helpBg.SetActive(true);
         help2.SetActive(true);
         }
 
         public void activateHelp3()
         {
+            help1.SetActive(false);
+            help2.SetActive(false);
+            helpBg.SetActive(true);
             help3.SetActive(true);
         }
 
@@ -27,6 +33,7 @@ public class HelpCanvas : MonoBehaviour
             help1.SetActive(false);
             help2.SetActive(false);
             help3.SetActive(false);
+            helpBg.SetActive(false);
         }   
 }
 
